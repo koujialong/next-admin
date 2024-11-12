4ade3d05be4b99b29330f9c19988ec87ac9d55fd
寇佳龙 <koujialong@bonc.com.cn>
2024-11-12 18:17:04 +0800

    fix: ui语法问题

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,6 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart"
 
-export const description = "An interactive bar chart"
-
 const chartData = [
   { date: "2024-04-01", desktop: 222, mobile: 150 },
   { date: "2024-04-02", desktop: 97, mobile: 180 },
